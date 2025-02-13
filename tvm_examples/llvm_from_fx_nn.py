62eac063e128373206974c95cd854ad6f508104f
Hugo Latendresse <hugolatendresse@gmail.com>
2025-02-12 22:22:35 -0500

    from fx working with both llvm and cuda

--- tvm_examples/llvm_from_fx_nn.py
+++ tvm_examples/llvm_from_fx_nn.py
@@ -33,27 +33,25 @@ class MyModule(torch.nn.Module):
 
 # Instantiate the model and create the input info dict.
 torch_model = MyModule()
+
+raw_data = np.random.rand(128,10).astype("float32")
 input_info = [((128, 10), "float32")]
-input_tensors = [
-    torch.as_tensor(np.random.randn(*shape).astype(dtype))
-    for shape, dtype in input_info
-]
+torch_data = torch.from_numpy(raw_data)
 
 # Use FX tracer to trace the PyTorch model.
 graph_module = fx.symbolic_trace(torch_model)
 
 mod: tvm.IRModule = from_fx(graph_module, input_info)
 
-mod_from_torch, params_from_torch = relax.frontend.detach_params(mod)
-print(mod.script())
+tvm_mod, tvm_params = relax.frontend.detach_params(mod)
+tvm_mod.show()
 
-exec = relax.build(mod_from_torch, target="llvm")
+exec = relax.build(tvm_mod, target="llvm")
 dev = tvm.cpu()
 vm = relax.VirtualMachine(exec, dev)
 
-raw_data = np.random.rand(128,10).astype("float32")
 tvm_input = tvm.nd.array(raw_data, dev)
 tvm_out = vm["main"](tvm_input).numpy()
-pytorch_out = torch_model(torch.from_numpy(raw_data)).detach().numpy() 
+pytorch_out = torch_model(torch_data).detach().numpy() 
 np.testing.assert_allclose(tvm_out, pytorch_out, rtol=1e-5, atol=1e-5) 
 print("Correctness test passed!") 
