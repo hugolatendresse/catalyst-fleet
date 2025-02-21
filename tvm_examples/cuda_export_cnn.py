326e08bf90b3bebfd82920c135e5d2dd01574d01
Hugo Latendresse <hugolatendresse@gmail.com>
2025-02-20 19:33:24 -0500

    test export and cuda works for cnn

--- tvm_examples/cuda_export_cnn.py
+++ tvm_examples/cuda_export_cnn.py
@@ -58,51 +58,7 @@ class PyTorchCNN(nn.Module):
 torch_model = PyTorchCNN().eval()
 
 raw_data = np.random.rand(1, 3, 128, 128).astype("float32")
-torch_data = torch.from_numpy(raw_data)
 
-# Give an example argument to torch.export
-example_args = (torch_data,)
+from hlutils.test_export_and_cuda import test_export_and_cuda
 
-# Convert the model to IRModule
-# TODO what does , unwrap_unit_return_tuple=True do? should we include?
-with torch.no_grad():
-    exported_program = export(torch_model, example_args)
-    mod_from_torch = from_exported_program(
-        exported_program, keep_params_as_input=True#, unwrap_unit_return_tuple=True
-    )
-
-tvm_mod, tvm_params = relax.frontend.detach_params(mod_from_torch)
-tvm_mod.show()
-
-from tvm import dlight as dl
-
-# tvm_mod = tvm.relax.transform.LegalizeOps()(tvm_mod)
-
-# with tvm.target.Target("cuda"):
-#     tvm_mod = dl.ApplyDefaultSchedule(
-#         dl.gpu.GEMV(),
-#         dl.gpu.LowBatchGEMV(),
-#         dl.gpu.Fallback(),
-#         dl.gpu.Matmul(),
-#         dl.gpu.Reduction(),
-#         dl.gpu.Transpose(),
-#         dl.gpu.GeneralReduction(),
-#         dl.gpu.RMSNorm(),
-#     )(tvm_mod)
-
-
-tvm_mod.show()
-
-target = tvm.target.Target.from_device(tvm.cuda())
-
-ex = relax.build(tvm_mod, target=target, pipeline=relax.get_default_pipeline(target))
-dev = tvm.device("cuda", 0)
-vm = relax.VirtualMachine(ex, dev)
-
-gpu_data = tvm.nd.array(raw_data, dev)
-gpu_params = [tvm.nd.array(p, dev) for p in tvm_params["main"]]
-gpu_out = vm["main"](gpu_data, *gpu_params)
-
-pytorch_out = torch_model(torch_data).detach().numpy() 
-np.testing.assert_allclose(gpu_out[0].numpy(), pytorch_out, rtol=1e-5, atol=1e-5) 
-print("Correctness test passed!") 
+test_export_and_cuda(raw_data, torch_model)
