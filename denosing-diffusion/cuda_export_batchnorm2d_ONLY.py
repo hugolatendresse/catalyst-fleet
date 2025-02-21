4a62f9c3b042439e7d0786e7673ec7c47e55c4eb
Hugo Latendresse <hugolatendresse@gmail.com>
2025-02-20 19:49:05 -0500

    batchnorm only failing on 1,2,2,2

--- denosing-diffusion/cuda_export_batchnorm2d_ONLY.py
+++ denosing-diffusion/cuda_export_batchnorm2d_ONLY.py
@@ -9,33 +9,27 @@ Correctness Test: FAIL
 """
 import sys
 sys.path.append('/ssd1/htalendr/tvm/python')
-from tvm import relax
 import numpy as np
-import tvm
-from tvm import relax
-import torch
 from torch import nn
-from torch.export import export
-from tvm.relax.frontend.torch import from_exported_program
-
-import torch.nn.functional as F
-import numpy as np
 from hlutils.test_export_and_cuda import test_export_and_cuda
 from hlutils.set_seed_all import set_seed_all
 
 set_seed_all()
 
+# What is batch norm? 
+# What is layer norm? Acros
+
 # Create a dummy model
 class PyTorchCNN(nn.Module):
     def __init__(self):
         super(PyTorchCNN, self).__init__()
-        self.bn1 = nn.BatchNorm2d(3)  # BatchNorm after conv1
+        self.bn1 = nn.BatchNorm2d(2)  # BatchNorm after conv1
 
     def forward(self, x):
         return self.bn1(x)
 
 torch_model = PyTorchCNN().eval()
 
-raw_data = np.random.rand(1, 3, 128, 128).astype("float32")
+raw_data = np.random.rand(1,2,2, 2).astype("float32")
 
 test_export_and_cuda(raw_data, torch_model)
