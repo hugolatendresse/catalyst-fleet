e05a294a93b8ef9e4a170da6fb37cd6684821037
Hugo Latendresse <hugolatendresse@gmail.com>
2025-03-08 13:50:03 -0500

    first unit test for chunk passes

--- denosing-diffusion/cuda_export_chunk.py
+++ denosing-diffusion/cuda_export_chunk.py
@@ -10,10 +10,14 @@ Correctness Test: ?
 
 import sys
 sys.path.append('/ssd1/htalendr/tvm/python') # Refer to local TVM build
-from torch import nn
+from tvm import relax
 import numpy as np
-
-from torch.nn import Softmax
+import tvm
+from tvm import relax
+import torch
+from torch.export import export
+from tvm.relax.frontend.torch import from_exported_program
+from torch import nn
 
 raw_data = np.random.rand(3, 4, 7, 11).astype("float32")
 
@@ -26,7 +30,39 @@ class ChunkModel(nn.Module):
     def forward(self, x):
         return x.chunk(self.chunks, dim=self.dim)
 
-torch_model = ChunkModel().eval()
+torch_model = ChunkModel(chunks=2, dim=1).eval()
 
-from hlutils.test_export_and_cuda import test_export_and_cuda
-test_export_and_cuda(raw_data, torch_model) 
+torch_data = torch.from_numpy(raw_data)
+
+# Give an example argument to torch.export
+example_args = (torch_data,)
+
+# Convert the model to IRModule
+# TODO what does , unwrap_unit_return_tuple=True do? should we include?
+with torch.no_grad():
+    exported_program = export(torch_model, example_args)
+    mod_from_torch = from_exported_program(
+        exported_program, keep_params_as_input=True#, unwrap_unit_return_tuple=True
+    )
+
+tvm_mod, tvm_params = relax.frontend.detach_params(mod_from_torch)
+
+target = tvm.target.Target.from_device(tvm.cuda())
+
+ex = relax.build(tvm_mod, target=target, relax_pipeline=relax.get_default_pipeline(target))
+dev = tvm.device("cuda", 0)
+vm = relax.VirtualMachine(ex, dev)
+
+gpu_data = tvm.nd.array(raw_data, dev)
+gpu_params = [tvm.nd.array(p, dev) for p in tvm_params["main"]]
+gpu_out = vm["main"](gpu_data, *gpu_params)
+
+torch_output = torch_model(torch_data)
+desired_0 = torch_output[0].detach().numpy()
+desired_1 = torch_output[1].detach().numpy()
+actual_0 = gpu_out[0].numpy()
+actual_1 = gpu_out[1].numpy()
+np.testing.assert_allclose(actual=actual_0, desired=desired_0, rtol=1e-5, atol=1e-5) 
+np.testing.assert_allclose(actual=actual_1, desired=desired_1, rtol=1e-5, atol=1e-5) 
+print("Correctness test passed!") 
+
--- denosing-diffusion/cuda_export_unet_decomposed_TORCH.py
+++ denosing-diffusion/cuda_export_unet_decomposed_TORCH.py
@@ -246,6 +246,8 @@ class ResnetBlock(Module):
             print("time_emb has shape", time_emb.shape)
             scale_shift = time_emb.chunk(2, dim = 1)
             print("scale_shift has shape", scale_shift[0].shape)
+            print("scale_shift has shape", scale_shift[1].shape)
+            print("scale_shift has shape", scale_shift[2].shape)
 
         h = self.block1(x, scale_shift = scale_shift)
 
@@ -1208,33 +1210,7 @@ print(time_data.dtype)
 
 example_args = (torch_data, time_data)
 
-# Convert the model to IRModule
-# TODO what does , unwrap_unit_return_tuple=True do? should we include?
-with torch.no_grad():
-    exported_program = export(torch_model, example_args)
-    mod_from_torch = from_exported_program(
-        exported_program, keep_params_as_input=True#, unwrap_unit_return_tuple=True
-    )
-
-tvm_mod, tvm_params = relax.frontend.detach_params(mod_from_torch)
-# tvm_mod.show()
-# tvm_params has one key, "main"
-# print("type tvm_params['main']:", type(tvm_params["main"]))
-# print("type(tvm_params['main'][0]):", type(tvm_params["main"][0]))
-# print("tvm_params keys:", list(tvm_params.keys()))
-
-target = tvm.target.Target.from_device(tvm.cuda())
-
-ex = relax.build(tvm_mod, target=target, relax_pipeline=relax.get_default_pipeline(target))
-dev = tvm.device("cuda", 0)
-vm = relax.VirtualMachine(ex, dev)
-
-gpu_data = tvm.nd.array(raw_data, dev)
-gpu_time_data = tvm.nd.array(time_data, dev)
-gpu_params = [tvm.nd.array(p, dev) for p in tvm_params["main"]]
-gpu_out = vm["main"](gpu_data, gpu_time_data, *gpu_params)
 
 pytorch_out = torch_model(torch_data, time_data).detach().numpy() 
-np.testing.assert_allclose(gpu_out[0].numpy(), pytorch_out, rtol=1e-5, atol=1e-5) 
-print("Correctness test passed!") 
+print("torch ran!") 
 
