cd3f4370becd26c7c761d4519ff419ae4f7047b9
Hugo Latendresse <hugolatendresse@gmail.com>
2025-03-24 12:54:22 -0400

    ok to export without dynamic control flow

--- whisper/whisper_encoder_inlined_truncated_torch_only.py
+++ whisper/whisper_encoder_inlined_truncated_torch_only.py
@@ -388,35 +388,29 @@ class WhisperEncoder(WhisperPreTrainedModel):
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        inputs_embeds = nn.functional.gelu(self.conv1(input_features))
+        inputs_embeds = nn.functional.gelu(self.conv2(inputs_embeds))
+
+        inputs_embeds = inputs_embeds.permute(0, 2, 1)
+        embed_pos = self.embed_positions.weight
+
+        hidden_states = inputs_embeds + embed_pos
+        hidden_states = nn.functional.dropout(hidden_states, p=self.dropout, training=self.training)
+
+        encoder_states = () if output_hidden_states else None
+        all_attentions = () if output_attentions else None
+
+        # check if head_mask has a correct number of layers specified if desired
+        if head_mask is not None:
+            assert head_mask.size()[0] == (
+                len(self.layers)
+            ), f"The head_mask should be specified for {len(self.layers)} layers, but it is for {head_mask.size()[0]}."
+
+        to_drop = False
+
         return input_features
-        # return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-        # inputs_embeds = nn.functional.gelu(self.conv1(input_features))
-        # inputs_embeds = nn.functional.gelu(self.conv2(inputs_embeds))
 
-        # inputs_embeds = inputs_embeds.permute(0, 2, 1)
-        # embed_pos = self.embed_positions.weight
-
-        # hidden_states = inputs_embeds + embed_pos
-        # hidden_states = nn.functional.dropout(hidden_states, p=self.dropout, training=self.training)
-
-        # encoder_states = () if output_hidden_states else None
-        # all_attentions = () if output_attentions else None
-
-        # # check if head_mask has a correct number of layers specified if desired
-        # if head_mask is not None:
-        #     assert head_mask.size()[0] == (
-        #         len(self.layers)
-        #     ), f"The head_mask should be specified for {len(self.layers)} layers, but it is for {head_mask.size()[0]}."
-
-        # for idx, encoder_layer in enumerate(self.layers):
-        #     if output_hidden_states:
-        #         encoder_states = encoder_states + (hidden_states,)
-        #     # add LayerDrop (see https://arxiv.org/abs/1909.11556 for description)
-        #     to_drop = False
-        #     if self.training:
-        #         dropout_probability = torch.rand([])
-        #         if dropout_probability < self.layerdrop:  # skip the layer
-        #             to_drop = True
 
         #     if to_drop:
         #         layer_outputs = (None, None)
@@ -478,3 +472,6 @@ print(exported_program)
 # from tvm.relax.frontend.torch import from_fx
 # graph_module = fx.symbolic_trace(torch_model)
 # print(graph_module)
+
+
+print("done exporting")
