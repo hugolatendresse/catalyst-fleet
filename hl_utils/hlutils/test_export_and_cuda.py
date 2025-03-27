13e5e980fe10df018019add9e58c62f4fecbfba9
Hugo Latendresse <hugolatendresse@gmail.com>
2025-03-27 08:41:13 -0400

    stack.default not supported

--- hl_utils/hlutils/test_export_and_cuda.py
+++ hl_utils/hlutils/test_export_and_cuda.py
@@ -9,7 +9,7 @@ from torch.export import export
 from tvm.relax.frontend.torch import from_exported_program
 
 
-def test_export_and_cuda(raw_data, torch_model, show=False):
+def test_export_and_cuda(raw_data, torch_model, show=False, debug=False):
     torch_data = torch.from_numpy(raw_data)
 
     # Give an example argument to torch.export
@@ -18,27 +18,41 @@ def test_export_and_cuda(raw_data, torch_model, show=False):
     # Convert the model to IRModule
     # TODO what does , unwrap_unit_return_tuple=True do? should we include?
     with torch.no_grad():
+        if debug:
+            print("Exporting model...")
         exported_program = export(torch_model, example_args)
+        if debug:
+            print("Converting model to IRModule...")
         mod_from_torch = from_exported_program(
             exported_program, keep_params_as_input=True#, unwrap_unit_return_tuple=True
         )
 
+    if debug:
+        print("Detaching parameters...")
     tvm_mod, tvm_params = relax.frontend.detach_params(mod_from_torch)
     if show:
         tvm_mod.show()
 
     target = tvm.target.Target.from_device(tvm.cuda())
 
+    if debug:
+        print("Defining VM...")
     ex = relax.build(tvm_mod, target=target, relax_pipeline=relax.get_default_pipeline(target))
     dev = tvm.device("cuda", 0)
     vm = relax.VirtualMachine(ex, dev)
 
+    if debug:
+        print("Running VM...")
     gpu_data = tvm.nd.array(raw_data, dev)
     gpu_params = [tvm.nd.array(p, dev) for p in tvm_params["main"]]
     gpu_out = vm["main"](gpu_data, *gpu_params)
 
+    if debug:
+        print("Running PyTorch model...")
     pytorch_out = torch_model(torch_data)
 
+    if debug:
+        print("Comparing outputs...")
     if isinstance(pytorch_out, tuple):
         for i in range(len(pytorch_out)):
             actual = gpu_out[i].numpy()
---
+++ whisper/cuda_export_index.py
@@ -0,0 +1,42 @@
+"""
+Model Type: index.Tensor
+Model Definition: PyTorch
+Model Export: torch.export
+Model Ingestion: tvm.relax.frontend.torch.from_exported_program
+Target: CUDA
+Compile and Run Test: ??
+Correctness Test: ??
+"""
+from tvm import relax
+import numpy as np
+import tvm
+from tvm import relax
+import torch
+from torch import nn
+from torch.export import export
+from tvm.relax.frontend.torch import from_exported_program
+import torch.nn.functional as F
+import numpy as np
+
+
+# Create a dummy model
+class IndexModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+    def forward(self, x):
+        val1 = x[1,4]
+        val2 = x[3,2]
+        val3 = x[5,6]
+        z = torch.stack([val1, val2, val3])
+        return z
+        
+
+torch_model = IndexModel().eval()
+
+raw_data = np.random.rand(10,10,10).astype("float32")
+
+from hlutils.test_export_and_cuda import test_export_and_cuda
+
+test_export_and_cuda(raw_data, torch_model)
